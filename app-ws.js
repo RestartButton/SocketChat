803d7a434d1ffd605d15d1a580acceff93816919
restartbutton <rogeriofbjr@hotmail.com>
2022-10-25 16:12:00 -0300

    Add Nickname and Rooms systems

--- app-ws.js
+++ app-ws.js
@@ -1,30 +1,46 @@
 module.exports = (server) => {
     const { Server } = require('socket.io');
     const io = new Server(server);
+    const clients = new Map();
 
     io.on('connection', (ws) => {
+
+        ws.on('room', data => {
+
+            if(clients.get(ws)){
+                console.log(`${ws.id} was disconnected from room ${clients.get(ws).room}`);
+                ws.leave(clients.get(ws).room);
+                clients.delete(ws);
+            }
+
+            console.log(`${ws.id} connected on room ${data}`);
+            const room = data;
+            clients.set(ws, { room });
+
+            ws.join(data);
+        });
 
         ws.on('chat message', data => {
             const message = JSON.parse(data);
             message.sender = ws.id;
 
-            console.log(`message from ${message.sender}: ` + message.msg);
+            console.log(`message from ${message.sender} on ${message.room}: ${message.msg}`);
 
             const outbound = JSON.stringify(message);
 
-            io.emit('chat message', outbound);
+            io.in(message.room).emit('chat message', outbound);
         });
 
         ws.on('read', data => {
             const message = JSON.parse(data);
             console.log(`message from ${message.sender} was read by ${ws.id}`);
 
-            io.emit('read', data);
+            io.in(message.room).emit('read', data);
         });
 
         ws.on('disconnect', () => {
 
-            console.log(`onClose: client disconnected!`);
+            console.log(`onClose: ${ws.id} disconnected!`);
 
         });
     });
--- assets/js/chat.js
+++ assets/js/chat.js
@@ -3,15 +3,29 @@ var nLidas = [];
 
 $('body').ready(function() {
     $('#botao_enviar').click(function () {
-        if($('#campo_mensagem').val().trim() != ''){
+        if($('#apelido_usuario').val() != '' && $('#codigo_sala').val() != '') {
+            if($('#campo_mensagem').val().trim() != ''){
 
-            let metadata = JSON.stringify({
-                msg: $('#campo_mensagem').val().trim(), 
-                time: new Date(),
-            })
+                let metadata = JSON.stringify({
+                    nick: $('#apelido_usuario').val().trim(),
+                    msg: $('#campo_mensagem').val().trim(), 
+                    time: new Date(),
+                    room: $('#codigo_sala').val().trim(),
+                });
 
-            ws.emit('chat message', metadata);
-            $('#campo_mensagem').val('');
+                ws.emit('chat message', metadata);
+                $('#campo_mensagem').val('');
+            }
+        } else {
+            alert('Ambos os campos Apelido e Código precisam ser preenchidos');
+        }
+    });
+
+    $('#codigo_sala').focusout(function() {
+        $('#codigo_sala').val($('#codigo_sala').val().trim());
+        if($('#codigo_sala').val().trim() != '') {
+            ws.emit('room', $('#codigo_sala').val().trim());
+            $('#janela_conversa').empty();
         }
     });
 
@@ -28,7 +42,7 @@ $('body').ready(function() {
 ws.on('chat message', (data) => { // data = { sender: ws.id, msg: metadata.msg, time: metadata.time };
     let metadata = JSON.parse(data);
     let enviado = new Date(metadata.time);
-    let mensagem = $(`<div class="balao_mensagem ${ metadata.sender == ws.id ? "eu" : "outro" }">${metadata.msg}<span>${enviado.getHours()}:${enviado.getMinutes()}:${enviado.getMilliseconds()}</span></div>`);
+    let mensagem = $(`<div class="balao_mensagem ${ metadata.sender == ws.id ? "eu" : "outro" }"><div class="mensagem">${metadata.nick}: ${metadata.msg}</div><span>${enviado.getHours()}:${enviado.getMinutes()}:${enviado.getMilliseconds()}</span></div>`);
     $('#janela_conversa').append(mensagem);
     if(metadata.sender != ws.id){
         if(!document.hidden) {
@@ -44,7 +58,7 @@ ws.on('read', (data) => {
     let enviado = new Date(metadata.time);
     if(metadata.sender == ws.id){
         $('.balao_mensagem').each(function() {
-            if($(this).text() == `${metadata.msg}${enviado.getHours()}:${enviado.getMinutes()}:${enviado.getMilliseconds()}` && $(this).find('span').text() == `${enviado.getHours()}:${enviado.getMinutes()}:${enviado.getMilliseconds()}`){
+            if($(this).find('.mensagem').text() == `${metadata.nick}: ${metadata.msg}` && $(this).find('span').text() == `${enviado.getHours()}:${enviado.getMinutes()}:${enviado.getMilliseconds()}`){
                 $(this).find('span').append(' - Visto');
             }
         });
